8f703b9681fcdac8fb38ae69847e1dc72d709d05
justinyugit <justin02yu@gmail.com>
2020-08-02 11:37:21 -0400

    adding swap usb feature - no need to reboot

--- RaspberryPiCode/cam.py
+++ RaspberryPiCode/cam.py
@@ -2,15 +2,19 @@ import picamera
 from time import sleep
 import time
 import datetime
-while True:
-    with picamera.PiCamera() as camera:
-        camera.framerate = 25
-        camera.resolution = (960,720)
-        
-        name = str(datetime.datetime.now())
-        #name = str(time.strfttime("%m%d%Y-%H%M%S"))
-        path = ("/mnt/usb/" + name + ".h264")
-        camera.start_recording(path)
-        sleep(1200)
-        camera.stop_recording()
+import os
+try:
+    while True:
+        with picamera.PiCamera() as camera:
+            camera.framerate = 25
+            camera.resolution = (960,720)
 
+            name = str(datetime.datetime.now())
+            #name = str(time.strfttime("%m%d%Y-%H%M%S"))
+            path = ("/mnt/usb/" + name + ".h264")
+            camera.start_recording(path)
+            sleep(1200)
+            camera.stop_recording()
+except:
+    os.system("./mounter.sh")
+    
